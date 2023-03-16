9552adc1e3b7f147121899387838e566cee3d5e3
cuznmop <cuzn1984@163.com>
2023-03-16 22:29:06 +0800

    P288

---
+++ student.cpp
@@ -0,0 +1,38 @@
+#include "Student.h"
+
+// 默认构造
+Student::Student()
+{
+
+}
+
+// 有参构造
+Student::Student(int id, string name, string pwd)
+{
+
+}
+
+// 菜单界面
+void Student::operMenu()
+{
+
+}
+
+// 申请预约
+void Student::applyOrder()
+{
+
+}
+
+// 查看自身预约
+void Student::showAllOrder()
+{
+
+}
+
+// 取消预约
+void Student::cancelOrder()
+{
+
+}
+
