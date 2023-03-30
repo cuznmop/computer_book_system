29aa74fd3f4bd55d1224bae7fd1a98d72e8c80ea
cuznmop <cuzn1984@163.com>
2023-03-30 23:00:50 +0800

    P291

---
+++ HeaderFile/globalFile.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// 管理员文件
+#define ADMIN_FILE "../txts/admin.txt"
+
+// 学生文件
+#define STUDENT_FILE "../txts/student.txt"
+
+// 教师文件
+#define TEACHER_FILE "../txts/teacher.txt"
+
+// 机房信息文件
+#define COMPUTER_FILE "../txts/computerRoom.txt"
+
+// 订单文件
+#define ORDER_FILE "../txts/order.txt" 
