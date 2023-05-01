70201fe670d209616a4488fddc993737bbb1ea79
cuznmop <cuzn1984@163.com>
2023-05-01 22:25:03 +0800

    P310

--- SourceFile/student.cpp
+++ SourceFile/student.cpp
@@ -172,6 +172,47 @@ void Student::showMyOrder()
 // 查看自身预约
 void Student::showAllOrder()
 {
+    OrderFile of;
+    if (of.m_Size == 0)
+    {
+        cout << "无预约记录!" << endl;
+        system("pause");
+        system("cls");
+        return;
+    }
+
+    for(int i=0; i<of.m_Size; i++)
+    {
+
+        cout << i+1 << ". ";
+        cout << "预约日期: 周" << of.m_orderData[i]["date"];
+        cout << " 时间段: " << (of.m_orderData[i]["interval"] == "1" ? "上午": "下午"); 
+        cout << " 学号: " << of.m_orderData[i]["stuId"];
+        cout << " 姓名: " << of.m_orderData[i]["stuName"];
+        cout << " 机房号: " << of.m_orderData[i]["roomId"];
+        string status = "状态: ";
+        // 1 审核中 2 已预约 -1 预约失败 0 取消预约
+        if(of.m_orderData[i]["status"] == "1" )
+        {
+            status += "审核中";
+        }
+        else if (of.m_orderData[i]["status"] == "2" )
+        {
+            status += "已预约";
+        }
+        else if (of.m_orderData[i]["status"] == "-1" )
+        {
+            status += "预约失败, 审核未通过";
+        }
+        else if (of.m_orderData[i]["status"] == "0" )
+        {
+            status += "预约已取消";
+        }
+        cout << status << endl;
+    }
+
+    system("pause");
+    system("cls");
 
 }
 
